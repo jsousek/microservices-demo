50acc215c4362c1a4f8238e08ddf6a07fecac9e4
Jiri Sousek <jirisousek@outlook.com>
2019-05-05 19:14:55 +0200

    project setup

---
+++ src/main/java/com/microservicesdemo/jsousek/JsousekApplication.java
@@ -0,0 +1,13 @@
+package com.microservicesdemo.jsousek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsousekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsousekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservicesdemo/jsousek/JsousekApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservicesdemo.jsousek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsousekApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
