e2d1292daaa9d92646c300fbbb35ce7c08bbf012
Jiri Sousek <jirisousek@outlook.com>
2019-05-07 17:45:12 +0200

    user model created

---
+++ src/main/java/com/microservicesdemo/jsousek/models/UserModel.java
@@ -0,0 +1,31 @@
+package com.microservicesdemo.jsousek.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+    @Id
+    @GeneratedValue
+    long id;
+    @NotNull
+    @Size(min = 4, max = 20, message = "username must be in between 4 and 20 characters long")
+    public String username;
+    @NotNull
+    @Size(min = 6, max = 36, message ="password must be in between 6 and 36 characters")
+    public String hashedPassword;
+    boolean isLogged;
+
+}
