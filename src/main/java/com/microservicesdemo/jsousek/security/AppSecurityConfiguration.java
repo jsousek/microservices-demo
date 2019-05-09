00079d4ee1ae28d172d1b9b4abab74bb530a0421
Jiri Sousek <jirisousek@outlook.com>
2019-05-09 20:08:01 +0200

    bcrypt encoder

--- src/main/java/com/microservicesdemo/jsousek/security/AppSecurityConfiguration.java
+++ src/main/java/com/microservicesdemo/jsousek/security/AppSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.microservicesdemo.jsousek.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +19,8 @@ public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .csrf().and()
-                .cors()
-                .and().logout()
+                .cors().and()
+                .logout()
                 .disable()
                 .authorizeRequests()
                 .antMatchers("/", "login", "register", "/mock/free").permitAll()
@@ -26,5 +29,10 @@ public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .addFilter(new JWTAuthenticationFilter(authenticationManager))
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(11);
     }
 }
