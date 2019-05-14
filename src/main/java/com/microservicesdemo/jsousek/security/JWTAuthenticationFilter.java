fbb8367366969ea8025a17bcc68d5b295cc831f4
Jiri Sousek <jirisousek@outlook.com>
2019-05-14 18:38:05 +0200

    aut filter

--- src/main/java/com/microservicesdemo/jsousek/security/JWTAuthenticationFilter.java
+++ src/main/java/com/microservicesdemo/jsousek/security/JWTAuthenticationFilter.java
@@ -1,13 +1,25 @@
 package com.microservicesdemo.jsousek.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservicesdemo.jsousek.models.UserModel;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.microservicesdemo.jsousek.security.SecurityConstants.*;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -21,7 +33,37 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest req,
                                                 HttpServletResponse res) throws AuthenticationException {
         try {
-            UserModel creds
+            UserModel user = new ObjectMapper()
+                    .readValue(req.getInputStream(), UserModel.class);
+
+            return  authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUsername(),
+                            user.getHashedPassword(),
+                            new ArrayList<>())
+            );
+        }
+        catch (IOException ex){
+            throw new RuntimeException(ex);
         }
     }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                           HttpServletResponse res,
+                                           FilterChain chain,
+                                           Authentication auth) throws IOException, ServletException {
+    String token = JWT.create()
+            .withSubject(((User)auth.getPrincipal()).getUsername())
+            .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .sign(Algorithm.HMAC512(SECRET.getBytes()));
+
+    res.addHeader(HEADER_STRING, appendToken(token));
+    }
+
+    private String appendToken(String token){
+        StringBuilder sBuilder = new StringBuilder(TOKEN_PREFIX);
+        return sBuilder.append(token).toString();
+    }
+
 }
---
+++ src/test/java/com/microservicesdemo/jsousek/security/JWTAuthenticationFilterTest.java
@@ -0,0 +1,14 @@
+package com.microservicesdemo.jsousek.security;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JWTAuthenticationFilterTest {
+
+    @Test
+    public void appendTokenTest(){
+
+    }
+
+}
