a3228e9caeea873b434469ed65d6b936fa6f06d5
Jiri Sousek <jirisousek@outlook.com>
2019-05-23 17:19:08 +0200

    registration endpoint refactored

--- src/main/java/com/microservicesdemo/jsousek/controllers/UserRestController.java
+++ src/main/java/com/microservicesdemo/jsousek/controllers/UserRestController.java
@@ -3,6 +3,7 @@ package com.microservicesdemo.jsousek.controllers;
 import com.microservicesdemo.jsousek.error_response.ErrorResponseService;
 import com.microservicesdemo.jsousek.models.UserModel;
 import com.microservicesdemo.jsousek.repositories.UserRepository;
+import com.microservicesdemo.jsousek.services.ResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.*;
 public class UserRestController extends BaseController{
     private UserRepository userRepository;
     private ErrorResponseService errorResponse;
+    private ResponseService responseService;
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserRestController(UserRepository userRepository, ErrorResponseService errorResponse) {
+    public UserRestController(UserRepository userRepository, ErrorResponseService errorResponse, ResponseService responseService) {
         this.userRepository = userRepository;
         this.errorResponse = errorResponse;
+        this.responseService = responseService;
     }
 
     @GetMapping("/")
@@ -37,7 +40,7 @@ public class UserRestController extends BaseController{
         }
         newUser.setHashedPassword(passwordEncoder.encode(newUser.hashedPassword));
         userRepository.save(newUser);
-        return ResponseEntity.ok("User registered successfully");
+        return ResponseEntity.ok(responseService.succesfulRegistrationResponse(newUser.getUsername()));
 
     }
 
--- src/main/java/com/microservicesdemo/jsousek/services/ResponseService.java
+++ src/main/java/com/microservicesdemo/jsousek/services/ResponseService.java
@@ -1,8 +1,14 @@
 package com.microservicesdemo.jsousek.services;
 
+import com.microservicesdemo.jsousek.models.ResponseModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ResponseService {
-    
+    public ResponseModel succesfulRegistrationResponse(String username){
+        return ResponseModel.builder()
+                .user(username)
+                .message("new user created with username: "+username)
+                .build();
+    }
 }
